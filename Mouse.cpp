074885c201d840386e31a44acadb7f92ea3d80f4
ESRENLL <esrenll@gmail.com>
2016-06-03 12:01:02 +0900

    add scan
    and router -> mouse

--- Mouse.cpp
+++ Mouse.cpp
@@ -6,13 +6,14 @@
 
 void Mouse::useTurn()
 {
-	std::pair<int, int> ddst = router.tremauxRouting(map, moveCount, curRow, curCol);
-	//printf("%d %d %d %d\n", dst.first, dst.second, d_dst.first, d_dst.second);
+	std::pair<int, int> ddst;
+	if(exitRow>=0) ddst = howToGo(exitRow, exitCol);
+	if(ddst.first+ddst.second==0) ddst = tremauxRouting();
 	move(ddst.first, ddst.second);	
-	//while(mana>=10){
-	//	// ADD : check using MouseJR
-	//	// ADD : control MouseJR and scan
-	//}
+	while(mana>=10){
+		std::pair<int, int> scanPoint = searchScanPoint();
+		scan(scanPoint.first, scanPoint.second);
+	}
 }
 
 void Mouse::move(int drow, int dcol)
@@ -49,77 +50,36 @@ void Mouse::move(int drow, int dcol)
 
 void Mouse::scan(int row, int col)
 {
-if(not mana >= 10){
-		return;
-	}
 	mana -= 10;
+	beginScanRow = row;
+	beginScanCol = col;
 
-	bool flagE = false;	// flag about finding exit point
-	bool flagR = false; // if end searching about rows
-	bool flagC = false;
-	// scan point
-	int prevRow=1;
-	int row=1;
-	int prevCol=0;
-	int col=0;
-	while(true){
-		if(flagE){	// if found exit
-			srand(time(NULL));
-			while(map[row][col] != MAP_FOG){
-				row = rand() % (map.getRows() - 3) + 1;
-				col = rand() % (map.getCols() - 3) + 1;
-			}
-		}
-		else{ // if didnt found exit
-			try{
-				if(!flagR && !flagC) // map's left side
-					row++;
-				else if(flagR && !flagC)	//map's down side
-					col++;
-				else if(flagR && flagC)	// map's right side
-					row--;
-				else
-					col--;	//map's up side
-
-				if(map[row][col] == MAP_FOG && map[prevRow][prevCol] == MAP_FOG)
-					break;
-				prevRow = row;
-				prevCol = col;
-				if(flagR && row == 0)
-					flagR = false;
-				if(!flagR && row == maze.getRows()-1)
-					flagR = true;
-				if(!flagC && col == maze.getCols()-1)
-					flagC = true;
-				if(map[row][col] == MAP_EMPTY)
-					flagE = true;
-			}
-			catch(const std::out_of_range& oor){ // if point is out of range
-					if(row > map.getRows()){
-						int resizeRows = std::max(map.getRows(), row+1);
-						int resizeCols = std::max(map.getCols(), col);
-						map.resize(resizeRows, resizeCols);
-					}
-					if (col > map.getCols()) {
-						int resizeRows = std::max(map.getRows(), row);
-						int resizeCols = std::max(map.getCols(), col + 1);
-						map.resize(resizeRows, resizeCols);
-					}
-			}
-		}
-	}
-	/*/
 	// update map scanned
-	for(int dy=-1; dy<=1; ++dy){
-		for(int dx=-1; dx<=1; ++dx){
-			if(maze.isIn(y+dy, x+dx))
-				map[y+dy][x+dx] = maze[y+dy][x+dx];
-			// 맵 범위를 벗어나면 맵 테두리를 알 수 있다.
-			// 이를 통해 출구를 발견할 수 있는데, 이를 쥐는 현재 알 방법이 없다.
-			// 방법??
+	for(int drow=-1; drow<=1; ++drow){
+		for(int dcol=-1; dcol<=1; ++dcol){
+			int trow = row+drow;
+			int tcol = col+dcol;
+			if(maze.isIn(trow, tcol)){
+				int resizeRows = std::max(map.getRows(), trow+1);
+				int resizeCols = std::max(map.getCols(), tcol+1);
+				map.resize(resizeRows, resizeCols);
+				moveCount.resize(map.getRows(), map.getCols());
+				map[trow][tcol] = maze[trow][tcol];
+				if(isEndOfMaze(trow, tcol, mazeRows, mazeCols)){
+					exitRow = trow;
+					exitCol = tcol;
+				}
+			}
+			else if(trow>=maze.getRows()) {
+				// discover maze rows end
+				mazeRows = maze.getRows();
+			}
+			else if(tcol>=maze.getCols()) {
+				// discover maze cols end
+				mazeCols = maze.getCols();
+			}
 		}
 	}
-	*/
 }
 
 int Mouse::getCurRow()
@@ -134,7 +94,12 @@ int Mouse::getCurCol()
 
 bool Mouse::isEscapeMaze()
 {
-	return curCol==0 || (curRow==0 && curCol!=1) || curCol==maze.getCols()-1 || curRow==maze.getRows()-1;
+	return isEndOfMaze(curRow, curCol, maze.getRows(), maze.getCols());
+}
+
+bool Mouse::isEndOfMaze(int row, int col, int rows, int cols)
+{
+	return map.isIn(row,col) && map[row][col]==MAP_EMPTY && (col==0 || (row==0 && col!=1) || col==cols-1 || row==rows-1);
 }
 
 int Mouse::getHealth()
@@ -162,4 +127,174 @@ void Mouse::printMap()
 		}
 		std::cout << '\n';
 	}
+}
+
+std::pair<int, int> Mouse::tremauxRouting()
+{
+	// return (drow,dcol)
+
+	Matrix predictedValue; // 3x3, [1][1] = current pos.
+	predictedValue.setInitValue(0);
+	predictedValue.resize(3,3);
+
+	// if adjacent moveCount==0 then go
+	// else pop stack and return
+	const int dir[4][2] = { {0,1}, {1,0}, {0,-1}, {-1,0} };
+	for(int d=0; d<4; ++d){
+		int trow = curRow + dir[d][0];
+		int tcol = curCol + dir[d][1];
+		if(map.isIn(trow, tcol) && map[trow][tcol]==MAP_EMPTY && moveCount[trow][tcol]==0)
+			predictedValue[dir[d][0]+1][dir[d][1]+1] = 1;
+	}
+	std::pair<int, int> maxRowCol = predictedValue.getMaxRowCol();
+	if(predictedValue[maxRowCol.first][maxRowCol.second]==0){
+		// adjacent moveCount==0 is not exist
+		// just back to stk
+		if(beginRowColStk.empty()) // no exit
+			return std::make_pair(-1, -1);
+		maxRowCol = beginRowColStk.top();
+		beginRowColStk.pop();
+		maxRowCol.first = abs(maxRowCol.first-2);
+		maxRowCol.second = abs(maxRowCol.second-2);
+	}
+	else {
+		// adjacent moveCount==0 is exist
+		// apply other algo, select
+
+		// prevent move to inner space
+		for(int d=0; d<4; ++d){
+			if(predictedValue[dir[d][0]+1][dir[d][1]+1]>0){
+				int temp = 0;
+				std::swap(temp, predictedValue[dir[d][0]+1][dir[d][1]+1]);
+				for(int row=-1; row<=1; ++row){
+					for(int col=-1; col<=1; ++col){
+						int trow = curRow + dir[d][0] + row;
+						int tcol = curCol + dir[d][1] + col;
+						if(map.isIn(trow,tcol) && map[trow][tcol]==MAP_WALL){
+							predictedValue[dir[d][0]+1][dir[d][1]+1] = temp;
+							break;
+						}
+					}
+				}
+			}
+		}
+		maxRowCol = predictedValue.getMaxRowCol();
+		beginRowColStk.push(maxRowCol);
+	}
+	maxRowCol.first--;
+	maxRowCol.second--;
+	return maxRowCol;
+}
+
+std::pair<int, int> Mouse::howToGo(int dstRow, int dstCol)
+{
+	// return drow, dcol
+
+	if(curRow==dstRow && curCol==dstCol) return std::make_pair(0,0);
+
+	const int beginCount = 10000;
+	const int dir[4][2] = { {1,0}, {0,1}, {-1,0}, {0,-1} }; // direction, (row,col)
+	std::stack< std::pair<int, int> > before, next; // next (row,col)
+	Matrix mapClone = map;
+
+	// find (x,y)
+	before.push(std::make_pair(curRow, curCol));
+	mapClone[curRow][curCol] = beginCount;
+	while(true){
+		if(before.empty()) return std::make_pair(0, 0); // cannot
+		
+		while(!before.empty()) {
+			std::pair<int, int> top = before.top();
+			if(top.first==dstRow && top.second==dstCol) break;
+			before.pop();
+			for(int d=0; d<4; ++d) {
+				int trow = top.first+dir[d][0];
+				int tcol = top.second+dir[d][1];
+				if(mapClone.isIn(trow,tcol) && mapClone[trow][tcol]==MAP_EMPTY) {
+					next.push(std::make_pair(trow,tcol));
+					mapClone[trow][tcol] = mapClone[top.first][top.second]+1;
+				}
+			}
+		}
+		if(!before.empty()) break;
+		if(next.empty()) return std::make_pair(0,0);
+		before.swap(next);
+	}
+	
+	// back from (x,y)
+	int backRow = dstRow;
+	int backCol = dstCol;
+	while(abs(backRow-curRow)+abs(backCol-curCol)!=1) {
+		for(int d=0; d<4; ++d) {
+			int trow = backRow+dir[d][0];
+			int tcol = backCol+dir[d][1];
+			if(mapClone.isIn(trow,tcol) && mapClone[trow][tcol]==mapClone[backRow][backCol]-1) {
+				backRow = trow;
+				backCol = tcol;
+				break;
+			}
+		}
+	}
+	return std::make_pair(backRow-curRow, backCol-curCol);
+}
+
+std::pair<int, int> Mouse::searchScanPoint()
+{
+	// 1. search exit (left side -> bottom side -> right side -> top side)
+	// 2. search way to current pos
+
+	std::pair<int, int> ret(0,0); // (row, col)
+	if(exitRow>=0) {
+		// exit searched
+		// use dfs, find empty adjacent fog 
+		const int dir[4][2] = { {1,0}, {0,1}, {-1,0}, {0,-1} }; // direction, (row,col)
+		Matrix chkPushed = map;
+		const int pushed = 10000;
+		std::stack< std::pair<int,int> > dfs;
+		chkPushed[exitRow][exitCol] = pushed;
+		dfs.push(std::make_pair(exitRow, exitCol));
+		while(!dfs.empty()){
+			std::pair<int, int> top = dfs.top();
+			dfs.pop();
+			for(int d=0; d<4; ++d){
+				int trow = top.first + dir[d][0];
+				int tcol = top.second + dir[d][1];
+				if(map.isIn(trow,tcol)){
+					if(map[trow][tcol]==MAP_FOG){
+						// scan point searched, return
+						ret.first = trow + trow - top.first;
+						ret.second = tcol + tcol - top.second;
+						return ret;
+					}
+					else if(chkPushed[trow][tcol]!=pushed){
+						chkPushed[trow][tcol] = pushed;
+						dfs.push(std::make_pair(trow, tcol));
+					}
+				}
+			}
+		}
+	}
+	else if(mazeRows<0) {
+		// search exit to left side(top->bottom)
+		ret.first = beginScanRow + 3;
+		ret.second = 0;
+	}
+	else if(mazeCols<0) {
+		// search exit to down side(left->right)
+		ret.first = mazeRows-1;
+		ret.second = beginScanCol + 3;
+	}
+	else if(beginScanRow>0) {
+		// search exit to right side(bottom->top)
+		ret.first = beginScanRow - 3;
+		if(ret.first<0) ret.first = 0;
+		ret.second = mazeCols-1;
+	}
+	else {
+		// search exit to top side(right->left)
+		ret.first = 0;
+		ret.second = beginScanCol - 3;
+		if(ret.second<0) ret.second = 0;
+	}
+	return ret;
 }
--- Mouse.h
+++ Mouse.h
@@ -1,20 +1,40 @@
 #pragma once
 
 #include "Matrix.h"
-#include "Router.h"
+#include <utility>
+#include <stack>
 
 class Mouse {
 private:
 	const Matrix maze;
 	Matrix map; // enum MAPSTATE { MAP_EMPTY=0, MAP_WALL, MAP_FOG };
 	Matrix moveCount; // 이동한 횟수(default 0)
-	Router router;
 	int curRow;
 	int curCol;
 	int health;
 	int mana;
+
+	// moues's view point
+	int exitRow; // default = -1 (undiscovered)
+	int exitCol; // default = -1 (undiscovered)
+	int mazeRows; // default = -1 (undiscovered)
+	int mazeCols; // default = -1 (undiscovered)
+
+private:
+	bool isEndOfMaze(int row, int col, int rows	, int cols); // by mouse
+	void move(int drow, int dcol); // 이동 후 맵(+moveCount, health, mana, x, y) 갱신
+	void scan(int row, int col); // 스캔 후 맵 및 마나 갱신
+	std::stack< std::pair<int, int> > beginRowColStk; // begin (row, col)
+	std::pair<int, int> tremauxRouting(); // return (row,col)
+	int beginScanRow, beginScanCol;
+	std::pair<int, int> searchScanPoint();
+	std::pair<int, int> howToGo(int dstRow, int dstCol); // (dstRow, dstCol)에 어떻게 가야하는지 drow, dcol 반환
+
 public:
-	Mouse(const Matrix& maze) : maze(maze), map(), curRow(0), curCol(1), health(2*maze.getRows()*maze.getCols()), mana(0) {
+	Mouse(const Matrix& maze)
+	: maze(maze), map(), curRow(0), curCol(1), health(2*maze.getRows()*maze.getCols()), mana(0)
+	, exitRow(-1), exitCol(-1), mazeRows(-1), mazeCols(-1), beginScanRow(0), beginScanCol(0)
+	{
 		map.setInitValue(MAP_FOG);
 		map.resize(2, 3);
 		moveCount.setInitValue(0);
@@ -27,8 +47,6 @@ public:
 	}
 	~Mouse() {}
 	void useTurn();
-	void move(int drow, int dcol); // 이동 후 맵(+moveCount, health, mana, x, y) 갱신
-	void scan(int row, int col); // 스캔 후 맵 및 마나 갱신
 	int getCurRow(); // 자신의 현재위치 x를 반환
 	int getCurCol(); // 자신의 현재위치 y를 반환
 	bool isEscapeMaze(); // 미로 탈출 여부 반환
